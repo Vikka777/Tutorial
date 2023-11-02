ef49e2e446e7c019b937e3716b794605e7173470
Vikka777 <96438170+Vikka777@users.noreply.github.com>
2023-11-02 14:35:09 +0200

    Delete hw_8.1 directory

--- hw_8.1/models.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import Document, StringField, BooleanField
-
-class Contact(Document):
-    full_name = StringField(required=True)
-    email = StringField(required=True)
-    sent_email = BooleanField(default=False)
